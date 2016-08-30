8a46a21be0c950ac09d381e46f0bbfe91cd3b233
laurachuburu <info@laurachuburu>
2016-08-30 13:04:44 -0300

    primer subida

---
+++ index.php
@@ -0,0 +1,62 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link href="favicon.ico" rel="shortcut icon">
+	<title>Charo Perelli - Objetos en hierro, cerámica y cemento</title>
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+	<meta property="og:image" content="imagenes/img-face.jpg" />
+	<script src="js/modernizr-2.5.3.js"></script>
+</head>
+
+<body class="home">
+	<header>
+		<nav>
+			<div class="container">
+				<ul>
+					<li><a href="">Objetos</a>
+						<ul>
+							<li><a href="">Objetos de jardín</a></li>
+							<li><a href="">Cemento</a></li>
+							<li><a href="">Cerámica</a></li>
+							<li><a href="">Vidrio</a></li>
+						</ul>
+					</li>
+					<li><a href="">Exposiciones</a>
+						<ul>
+							<li><a href="">La vidriera</a></li>
+							<li><a href="">Habitantes del jardín</a></li>
+							<li><a href="">Otras exposiciones</a></li>
+						</ul>
+					</li>
+					<li><a href="">Cursos</a></li>
+					<li><a href="">Ventas</a></li>
+					<li><a href="">El taller</a></li>
+					<li><a href="">Contacto</a></li>
+				</ul>
+				<a class="facebook" href="http://www.facebook.com/charo.perellitaller" target="_blank">Charo Perelli en Facebook</a>
+			</div><!--cierra container-->
+		</nav>
+		<div class="logo">
+			<img src="images/firma-home.png" alt="">
+			<h1>Hierro, cerámica, cemento y vidrio</h1>
+		</div>
+	</header>
+
+	
+	<p><a class="laura" href="http://www.laurachuburu.com.ar" target="_blank">Diseñado por www.laurachuburu.com.ar</a></p>
+
+	<!--google analytics-->
+		<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+		var pageTracker = _gat._getTracker("UA-4834851-1");
+		pageTracker._initData();
+		pageTracker._trackPageview();
+		</script>
+	<!--fin google analytics-->
+</body>
+</html>
