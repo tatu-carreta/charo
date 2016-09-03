617e2aba4720a2c540664f7bee61898bb6a8b9d8
eugeMolinero <euge_molinero@yahoo.com.ar>
2016-09-03 15:50:40 -0300

    Menu + Bootstrap

--- index.php
+++ index.php
@@ -4,37 +4,63 @@
 	<meta charset="UTF-8">
 	<link href="favicon.ico" rel="shortcut icon">
 	<title>Charo Perelli - Objetos en hierro, cerámica y cemento</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/styles.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 	<meta property="og:image" content="imagenes/img-face.jpg" />
+	<!-- M O D E R N I Z R -->
 	<script src="js/modernizr-2.5.3.js"></script>
+	
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body class="home">
 	<header>
-		<nav>
+		<nav class="navbar navbar-default" role="navigation">
 			<div class="container">
-				<ul>
-					<li><a href="">Objetos</a>
-						<ul>
-							<li><a href="">Objetos de jardín</a></li>
-							<li><a href="">Cemento</a></li>
-							<li><a href="">Cerámica</a></li>
-							<li><a href="">Vidrio</a></li>
-						</ul>
-					</li>
-					<li><a href="">Exposiciones</a>
-						<ul>
-							<li><a href="">La vidriera</a></li>
-							<li><a href="">Habitantes del jardín</a></li>
-							<li><a href="">Otras exposiciones</a></li>
-						</ul>
-					</li>
-					<li><a href="">Cursos</a></li>
-					<li><a href="">Ventas</a></li>
-					<li><a href="">El taller</a></li>
-					<li><a href="">Contacto</a></li>
-				</ul>
+				<div class="navbar-header">
+				    <button type="button" class="navbar-toggle" data-toggle="collapse"
+				            data-target=".navbar-ex1-collapse">
+				    	<span class="sr-only">Desplegar navegación</span>
+				    	<span class="icon-bar"></span>
+				    	<span class="icon-bar"></span>
+				    	<span class="icon-bar"></span>
+				    </button>
+				    <a class="navbar-brand" href="#">Logotipo</a>
+				</div>
+
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          						Objetos <b class="caret"></b>
+        					</a>
+							<ul class="dropdown-menu">
+								<li><a href="">Objetos de jardín</a></li>
+								<li><a href="">Cemento</a></li>
+								<li><a href="">Cerámica</a></li>
+								<li><a href="">Vidrio</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          						Exposiciones <b class="caret"></b>
+        					</a>
+							<ul class="dropdown-menu">
+								<li><a href="">La vidriera</a></li>
+								<li><a href="">Habitantes del jardín</a></li>
+								<li><a href="">Otras exposiciones</a></li>
+							</ul>
+						</li>
+						<li><a href="">Cursos</a></li>
+						<li><a href="">Ventas</a></li>
+						<li><a href="">El taller</a></li>
+						<li><a href="">Contacto</a></li>
+					</ul>
+				</div>
 				<a class="facebook" href="http://www.facebook.com/charo.perellitaller" target="_blank">Charo Perelli en Facebook</a>
 			</div><!--cierra container-->
 		</nav>
