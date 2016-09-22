9ced11ca632a5b9c3024576cd706a4388d011e2e
laurachuburu <info@laurachuburu>
2016-09-22 11:34:18 -0300

    login y admin-listado

---
+++ admin-listado.php
@@ -0,0 +1,162 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Taller de Charo Perelli - Objetos en hierro, cemento, cerámica y vidrio.</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="Venta de objetos de jardín y decoración. Cursos de cerámica, mosaico y cemento en Gonnet, La Plata.">
+	<meta property="og:image" content="images/img-face.jpg">
+	<link href="favicon.ico" rel="shortcut icon">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|Roboto:300,400,500,700" rel="stylesheet">
+	<script src="js/modernizr-2.5.3.js"></script>	
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Fancybox -->
+	<script type="text/javascript" src="js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+	<link rel="stylesheet" href="js/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+	<script type="text/javascript" src="js/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+</head>
+
+<body class="interiores">
+	<header>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+				    <button type="button" class="navbar-toggle" data-toggle="collapse"
+				            data-target=".navbar-ex1-collapse">
+				    	<span class="sr-only">Desplegar navegación</span>
+				    	<span class="icon-bar"></span>
+				    	<span class="icon-bar"></span>
+				    	<span class="icon-bar"></span>
+				    </button>
+				    <!-- <a class="navbar-brand" href="#">Logotipo</a> -->
+				</div>
+
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          						Objetos <b class="caret"></b>
+        					</a>
+							<ul class="dropdown-menu">
+								<li><a href="catalogo.php">Objetos de jardín</a></li>
+								<li><a href="catalogo.php">Cemento</a></li>
+								<li><a href="catalogo.php">Cerámica</a></li>
+								<li><a href="catalogo.php">Vidrio</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          						Exposiciones <b class="caret"></b>
+        					</a>
+							<ul class="dropdown-menu">
+								<li><a href="catalogo.php">La vidriera</a></li>
+								<li><a href="catalogo.php">Habitantes del jardín</a></li>
+								<li><a href="catalogo.php">Otras exposiciones</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          						Cursos <b class="caret"></b>
+        					</a>
+							<ul class="dropdown-menu">
+								<li><a href="cursos.php">Los cursos</a></li>
+								<li><a href="catalogo.php">Trabajos de alumnos</a></li>
+							</ul>
+						</li>
+						<li><a href="ventas.php">Ventas</a></li>
+						<li><a href="taller.php">El taller</a></li>
+						<li><a href="contacto.php">Contacto</a></li>
+					</ul>
+				</div>
+				<a class="facebook" href="http://www.facebook.com/charo.perellitaller" target="_blank">Charo Perelli en Facebook</a>
+			</div><!--cierra container-->
+		</nav>
+		<div class="logo">
+			<a href="index.php"><img src="images/firma-home.png" alt="Charo Perelli"></a>
+			<h1>Hierro, cemento, cerámica y vidrio</h1>
+		</div>
+	</header>
+
+	<section>
+		<div class="container catalogo">
+			<h2>Objetos de jardín</h2>
+
+			<a href="" class="btn-nuevo-grupo">Nuevo grupo</a>
+
+			<div class="titulo-grupo">
+				<h3>Hierro y cemento</h3>
+				<a href="" class="btn-azul-admin">Agregar foto</a>
+				<a href="" class="btn-iconos cambiar">Cambiar nombre de grupo</a>
+				<a href="" class="btn-iconos eliminar">Eliminar grupo</a>
+			</div>
+			<ul>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/bolas005.jpg" title="Objetos de jardín / 01"><img src="images/objetos/miniaturas/objetos-jardin/bolas005.jpg"></a><p>Objetos de jardín / 01</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/casam2.jpg" title="Objetos de jardín / 02"><img src="images/objetos/miniaturas/objetos-jardin/casam2.jpg"></a><p>Objetos de jardín / 02</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/casam4.jpg" title="Objetos de jardín / 03"><img src="images/objetos/miniaturas/objetos-jardin/casam4.jpg"></a><p>Objetos de jardín / 03</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/cvxz.jpg" title="Objetos de jardín / 04"><img src="images/objetos/miniaturas/objetos-jardin/cvxz.jpg"></a><p>Objetos de jardín / 04</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC00688.JPG" title="Objetos de jardín / 05"><img src="images/objetos/miniaturas/objetos-jardin/DSC00688.JPG"></a><p>Objetos de jardín / 05</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC01229.JPG" title="Objetos de jardín / 06"><img src="images/objetos/miniaturas/objetos-jardin/DSC01229.JPG"></a><p>Objetos de jardín / 06</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC01231.JPG" title="Objetos de jardín / 07"><img src="images/objetos/miniaturas/objetos-jardin/DSC01231.JPG"></a><p>Objetos de jardín / 07</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC02331.JPG" title="Objetos de jardín / 08"><img src="images/objetos/miniaturas/objetos-jardin/DSC02331.JPG"></a><p>Objetos de jardín / 08</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC02332.JPG" title="Objetos de jardín / 09"><img src="images/objetos/miniaturas/objetos-jardin/DSC02332.JPG"></a><p>Objetos de jardín / 09</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC03158.JPG" title="Objetos de jardín / 10"><img src="images/objetos/miniaturas/objetos-jardin/DSC03158.JPG"></a><p>Objetos de jardín / 10</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC03160.JPG" title="Objetos de jardín. Paisajismo Ivana Gervasio / 11"><img src="images/objetos/miniaturas/objetos-jardin/DSC03160.JPG"></a><p>Objetos de jardín / 11</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC03161-2.JPG" title="Objetos de jardín. Paisajismo Ivana Gervasio / 12"><img src="images/objetos/miniaturas/objetos-jardin/DSC03161-2.JPG"></a><p>Objetos de jardín / 12</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC03698.JPG" title="Objetos de jardín / 13"><img src="images/objetos/miniaturas/objetos-jardin/DSC03698.JPG"></a><p>Objetos de jardín / 13</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC03863.JPG" title="Objetos de jardín / 14"><img src="images/objetos/miniaturas/objetos-jardin/DSC03863.JPG"></a><p>Objetos de jardín / 14</p></li>
+				
+			</ul>
+			<div class="titulo-grupo">
+				<h3>Hierro y cerámica</h3>
+				<a href="" class="btn-azul-admin">Agregar foto</a>
+				<a href="" class="btn-iconos cambiar">Cambiar nombre de grupo</a>
+				<a href="" class="btn-iconos eliminar">Eliminar grupo</a>
+			</div>
+			<ul>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC03899.JPG" title="Objetos de jardín / 15"><img src="images/objetos/miniaturas/objetos-jardin/DSC03899.JPG"></a><p>Objetos de jardín / 15</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC04206.JPG" title="Objetos de jardín / 16"><img src="images/objetos/miniaturas/objetos-jardin/DSC04206.JPG"></a><p>Objetos de jardín / 16</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC04690.JPG" title="Objetos de jardín / 17"><img src="images/objetos/miniaturas/objetos-jardin/DSC04690.JPG"></a><p>Objetos de jardín / 17</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC04692.JPG" title="Objetos de jardín / 18"><img src="images/objetos/miniaturas/objetos-jardin/DSC04692.JPG"></a><p>Objetos de jardín / 18</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC06698.jpg" title="Objetos de jardín / 19"><img src="images/objetos/miniaturas/objetos-jardin/DSC06698.jpg"></a><p>Objetos de jardín / 19</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC04921.JPG" title="Objetos de jardín / 20"><img src="images/objetos/miniaturas/objetos-jardin/DSC04921.JPG"></a><p>Objetos de jardín / 20</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC04930.JPG" title="Objetos de jardín / 21"><img src="images/objetos/miniaturas/objetos-jardin/DSC04930.JPG"></a><p>Objetos de jardín / 21</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC06695.jpg" title="Objetos de jardín / 22"><img src="images/objetos/miniaturas/objetos-jardin/DSC06695.jpg"></a><p>Objetos de jardín / 22</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC06232.jpg" title="Objetos de jardín / 23"><img src="images/objetos/miniaturas/objetos-jardin/DSC06232.jpg"></a><p>Objetos de jardín / 23</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC05529.JPG" title="Objetos de jardín / 24"><img src="images/objetos/miniaturas/objetos-jardin/DSC05529.JPG"></a><p>Objetos de jardín / 24</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC05530.JPG" title="Objetos de jardín / 25"><img src="images/objetos/miniaturas/objetos-jardin/DSC05530.JPG"></a><p>Objetos de jardín / 25</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/DSC05547.JPG" title="Objetos de jardín / 26"><img src="images/objetos/miniaturas/objetos-jardin/DSC05547.JPG"></a><p>Objetos de jardín / 26</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/expo1.JPG" title="Objetos de jardín / 27"><img src="images/objetos/miniaturas/objetos-jardin/expo1.JPG"></a><p>Objetos de jardín / 27</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/expo2.JPG" title="Objetos de jardín / 28"><img src="images/objetos/miniaturas/objetos-jardin/expo2.JPG"></a><p>Objetos de jardín / 28</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/expo3.JPG" title="Objetos de jardín / 29"><img src="images/objetos/miniaturas/objetos-jardin/expo3.JPG"></a><p>Objetos de jardín / 29</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/foto_jardin11.jpg" title="Objetos de jardín / 30"><img src="images/objetos/miniaturas/objetos-jardin/foto_jardin11.jpg"></a><p>Objetos de jardín / 30</p></li>
+				<li><a class="fancybox" rel="group" href="images/objetos/ampliaciones/objetos-jardin/fotos012.jpg" title="Objetos de jardín / 31"><img src="images/objetos/miniaturas/objetos-jardin/fotos012.jpg"></a><p>Objetos de jardín / 31</p></li>
+			</ul>
+			<script type="text/javascript">
+				$(document).ready(function() {
+					$(".fancybox").fancybox();
+				});
+			</script>
+		</div><!--cierra container-->
+	</section>
+	<footer>
+		<p class="firma">
+			Copyright © Charo Perelli 2016 - Desarrollo web: <a href="http://www.laurachuburu.com.ar/" target="_blank">www.laurachuburu.com.ar</a>
+		</p>
+	</footer>
+
+	<!--google analytics-->
+		<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+		var pageTracker = _gat._getTracker("UA-4834851-1");
+		pageTracker._initData();
+		pageTracker._trackPageview();
+		</script>
+	<!--fin google analytics-->
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Acceso Charo</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link href="favicon.ico" rel="shortcut icon">
+	<link href="css/styles.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700|Roboto:300,400,500,700" rel="stylesheet">
+	<script src="js/modernizr-2.5.3.js"></script>	
+</head>
+
+<body class="login">
+	<div class="logo">
+		<img src="images/firma-home.png" alt="Charo">
+
+				<form action="" method="" class="form-login"> 			  
+					<label>Usuario<br>
+						<input name="" type="text" class="campo-form" id="" required /></label><br>
+					<label>Clave<br>
+						<input name="" type="password" class="campo-form" id="" required /></label><br>
+					<label><input type="submit" id="button" value="Entrar"></label>
+				</form>
+
+	</div>
+</body>
+</html>
